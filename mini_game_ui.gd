e7c1f898909b11d5d3be379e0874450d5f487654
Tabatha Williams <geltta@appstate.edu>
2026-03-26 14:45:13 -0400

    added basic screen to for popping up when player enters minigame

---
+++ mini_game_ui.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	#Close the game if they press escape
+	if event.is_action_pressed("ui_cancel"):
+		queue_free()
