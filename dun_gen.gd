0f0ed81888b0b565b50fd0cba911383f235fa0d3
dr9in9an9 <sisniega.serrano@gmail.com>
2026-05-03 20:31:16 -0400

    newest.

--- dun_gen.gd
+++ dun_gen.gd
@@ -42,12 +42,7 @@ func grid_debug():
 	
 	print(str)
 
-func get_neighbor_coords(coords : Vector2, dir):
-	if ((coords.x - 1) > 0) or ((coords.x + 1) <= grid_size):
-		print("gncoord OOB x")
-	if ((coords.y - 1) > 0) or ((coords.y + 1) <= grid_size):
-		print("gncoord OOB y")
-	
+func get_neighbor_coords(coords : Vector2, dir) -> Vector2:
 	if dir == "front":
 		return Vector2(coords.x, coords.y - 1)
 	if dir == "back":
@@ -57,20 +52,61 @@ func get_neighbor_coords(coords : Vector2, dir):
 	if dir == "right":
 		return Vector2(coords.x + 1, coords.y)
 	print("gnc failed. direction input likely invalid.")
+	return Vector2(NAN, NAN) #not a number
 
-func get_neighbor_chunk(coords : Vector2, dir):
-	if ((coords.x - 1) < 0) or ((coords.x + 1) >= grid_size):
-		print("gnchunk OOB x")
-		return 0
-	if ((coords.y - 1) < 0) or ((coords.y + 1) >= grid_size):
-		print("gnchunk OOB y")
-		return 0
-	var neighbor = get_neighbor_coords(coords, dir)
-	return chunk_grid[neighbor.x][neighbor.y]
+func get_chunk(coords : Vector2): #-> Chunk:
+	if !(0 <= coords.x and coords.x < grid_size):
+		return null
+	if !(0 <= coords.y and coords.y < grid_size):
+		return null
+	
+	if check_if_empty_coords(coords):
+		return null
+	
+	return chunk_grid[coords.x][coords.y]
+	
+func get_surrounding_chunks(coords : Vector2):	
+	var surrounding_chunks : Dictionary = { 
+		"front" : get_chunk(get_neighbor_coords(coords, "front")), 
+		"right" : get_chunk(get_neighbor_coords(coords, "right")),
+		"back" : get_chunk(get_neighbor_coords(coords, "back")),
+		"left" : get_chunk(get_neighbor_coords(coords, "left"))
+		}
+	
+	return surrounding_chunks
+
+func get_surrounding_connectors(coords : Vector2):
+	var surrounding_chunks = get_surrounding_chunks(coords)
+	
+	var connections_to_match = []
+	
+	if surrounding_chunks["front"] == null:
+		connections_to_match.append(null) 
+	else:
+		var cons = surrounding_chunks["front"].get_connections()
+		connections_to_match.append(cons["back"])
+	if surrounding_chunks["right"] == null:
+		connections_to_match.append(null) 
+	else:
+		var cons = surrounding_chunks["right"].get_connections()
+		connections_to_match.append(cons["left"])
+	if surrounding_chunks["back"] == null:
+		connections_to_match.append(null) 
+	else:
+		var cons = surrounding_chunks["back"].get_connections()
+		connections_to_match.append(cons["front"])
+	if surrounding_chunks["left"] == null:
+		connections_to_match.append(null) 
+	else:
+		var cons = surrounding_chunks["left"].get_connections()
+		connections_to_match.append(cons["right"])
+		
+	return connections_to_match
 
 func check_if_empty_coords(coords : Vector2) -> bool:
-	if(coords.y < 0 or coords.x < 0):
-		print("OOBs")
+	if !(0 <= coords.x and coords.x < grid_size):
+		return false
+	if !(0 <= coords.y and coords.y < grid_size):
 		return false
 
 	if chunk_grid[coords.x][coords.y] is int:
@@ -81,51 +117,38 @@ func check_if_empty_coords(coords : Vector2) -> bool:
 func check_if_connectors_fit(chunk : Chunk, potential_coords : Vector2) -> Array: 
 	var possible_turns : Array = []
 	
-	var surrounding_chunks : Dictionary = { 
-		"front" : get_neighbor_chunk(chunk.get_coords(), "front"), 
-		"right" : get_neighbor_chunk(chunk.get_coords(), "right"),
-		"back" : get_neighbor_chunk(chunk.get_coords(), "back"),
-		"left" : get_neighbor_chunk(chunk.get_coords(), "left")
-		}
+	var connections_to_match = get_surrounding_connectors(potential_coords)
 	
-	var connections_to_match = []
-	if surrounding_chunks["front"] == 0:
-		connections_to_match.append(null) 
-	else:
-		var cons = surrounding_chunks["front"].get_connections()
-		connections_to_match.append(cons["back"])
-	if surrounding_chunks["right"] == 0:
-		connections_to_match.append(null) 
-	else:
-		var cons = surrounding_chunks["right"].get_connections()
-		connections_to_match.append(cons["left"])
-	if surrounding_chunks["back"] == 0:
-		connections_to_match.append(null) 
-	else:
-		var cons = surrounding_chunks["back"].get_connections()
-		connections_to_match.append(cons["front"])
-	if surrounding_chunks["left"] == 0:
-		connections_to_match.append(null) 
-	else:
-		var cons = surrounding_chunks["left"].get_connections()
-		connections_to_match.append(cons["right"])
+	print(connections_to_match)
 	
 	for turn in range(4):
-		var are_matching = true
+		print(turn)
+		var are_matching = false
 		
-		#var current_connections : Dictionary[String, Connector] = chunk.get_connections()
-		#if connections_to_match[0] == null:
-			#if current_connections["front"] != null:
-				#are_matching = false
-		#if connections_to_match[1] == null:
-			#if current_connections["right"] != null:
-				#are_matching = false
-		#if connections_to_match[2] == null:
-			#if current_connections["back"] != null:
-				#are_matching = false
-		#if connections_to_match[3] == null:
-			#if current_connections["left"] != null:
-				#are_matching = false
+		var current_connections : Dictionary[String, Connector] = chunk.get_connections()
+		if connections_to_match[0] != null:
+			if current_connections["front"] != null:
+				are_matching = true
+			else: 
+				are_matching = false
+		if connections_to_match[1] != null:
+			if current_connections["right"] != null:
+				are_matching = true
+			else: 
+				are_matching = false
+		if connections_to_match[2] != null:
+			if current_connections["back"] != null:
+				are_matching = true
+			else: 
+				are_matching = false
+		if connections_to_match[3] != null:
+			if current_connections["left"] != null:
+				are_matching = true
+			else: 
+				are_matching = false
+		
+		#print(current_connections.values())
+		print(are_matching)
 		
 		if are_matching: 
 			possible_turns.append(turn)
@@ -154,6 +177,12 @@ func chunk_spawn(scene_index : int, coords : Vector2) -> bool:
 		chunk.set_orientation(possible_turns[selection])
 		
 		chunk.position = Vector3(coords.x*chunk_size, 0, coords.y*chunk_size)
+		
+		var sur_con = get_surrounding_connectors(coords)
+		for con in sur_con:
+			if con != null:
+				pass
+				##################
 		
 		return true
 	
